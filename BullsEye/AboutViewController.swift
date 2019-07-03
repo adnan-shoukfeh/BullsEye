7cf604ccebb5edacd1092ea415c715649ce92f6b
Adnan Shoukfeh <sadnan@umich.edu>
2019-07-03 15:22:03 -0400

    Added new screens(view controllers)

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,22 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Adnan Shoukfeh on 7/2/19.
+//  Copyright © 2019 Adnan Shoukfeh. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+
+}
